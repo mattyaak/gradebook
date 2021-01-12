872129c66d0993d71f05bfdba581e76c5db06474
Akos Mattyasovszky <to.akos@gmail.com>
2021-01-12 16:50:51 +0100

    y

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace gradebook
 {
@@ -6,7 +7,22 @@ namespace gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            
+
+            grades.Add(56.1);
+
+            var result = 0.0;
+
+            foreach (var number in grades)
+            {
+                result += number;
+                    
+            }
+      
+            result /= grades.Count;
+            Console.WriteLine($"The number is {result:N1}");
         }
     }
 }
